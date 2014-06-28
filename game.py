172eb9aa38678bdcc3158818fea864fa8b322b55
miracode <rascatim@gmail.com>
2014-06-28 13:22:16 -0700

    begin game outline

---
+++ game.py
@@ -0,0 +1,41 @@
+"""Aliens have invaded a space ship and our hero has 
+to go through a maze of rooms defeating them so he 
+can escape into an escape pod to the planet below. The 
+game will be more like a Zork or Adventure type game 
+with text outputs and funny ways to die. The game will 
+involve an engine that runs a map full of rooms or scenes. 
+Each room will print its own description when the player 
+enters it and then tell the engine what room to run next out 
+of the map."""
+
+### SCENES ###
+# DEATH - when player dies
+# CENTRAL CORRIDOR - Starting point with Gothon and joke
+# LASER WEAPON ARMORY - Where hero gets bomb
+# THE BRIDGE - Battle scene where hero places the bomb
+# ESCAPE POD - Where hero escapes after guessing the right one
+
+### NOUNS ###
+# Alien
+# Player
+# Ship
+# Maze
+# Room 
+# Scene 
+# Gothon 
+# Escape Pod 
+# Planet 
+# Map 
+# Engine 
+# Death 
+# Central Corridor 
+# Laser Weapon Armory 
+# The Bridge 
+
+### VERBS ###
+# Invaded 
+# Go through maze 
+# Defeat 
+# Escape 
+# Print description
+# Run room

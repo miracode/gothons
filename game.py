2f11d867fc3502dd760cd9785c1da6992b4702ad
miracode <rascatim@gmail.com>
2014-06-28 13:37:03 -0700

    class outline

--- game.py
+++ game.py
@@ -34,3 +34,60 @@ of the map."""
 # Engine 
     # - play
 
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+class Scene(object):
+
+    def enter(self):
+        pass
+
+
+class Engine(object):
+
+    def __init__(slef, scene_map):
+        pass
+
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass 
+
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
